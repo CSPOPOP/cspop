bc754f84651a4e1ed8c2b534644c93765d0aa625
kw99j10 <foryou0084@gmail.com>
2023-05-05 10:40:55 +0900

    fix: 졸업자 조회 다운로드 기능 재구현

--- src/main/java/kyonggi/cspop/application/controller/board/excel/graduate/GraduateCheckController.java
+++ src/main/java/kyonggi/cspop/application/controller/board/excel/graduate/GraduateCheckController.java
@@ -29,8 +29,14 @@ import kyonggi.cspop.domain.schedule.service.ScheduleService;
 import kyonggi.cspop.domain.users.Users;
 import kyonggi.cspop.domain.users.service.UsersService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -39,7 +45,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -170,11 +178,101 @@ public class GraduateCheckController {
         return ResponseEntity.ok().build();
     }
 
+    @SneakyThrows
+    @GetMapping("api/graduation/graduate_management.download")
+    public ResponseEntity<InputStreamResource> downloadExcel(HttpServletResponse response) {
+        File tmpFile = getTmpFile();
+        InputStream excelFile = getExcelFile(tmpFile);
+        return ResponseEntity.ok().contentLength(tmpFile.length()).contentType(MediaType.APPLICATION_OCTET_STREAM).header("Content-Disposition", "attachment;filename=graduation.xlsx").body(new InputStreamResource(excelFile));
+    }
+
     /**
      * 프론트 작업자는 이 밑으로 로직 안봐도 됩니다.
      * 위의 public 접근 제어자 메서드만 확인해 주세요.
      */
 
+    private File getTmpFile() throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+        File tmpFile = getFile(workbook);
+        OutputStream fos = new FileOutputStream(tmpFile);
+        workbook.write(fos);
+        return tmpFile;
+    }
+
+    private static InputStream getExcelFile(File tmpFile) throws FileNotFoundException {
+        InputStream res = new FileInputStream(tmpFile) {
+            @Override
+            public void close() throws IOException {
+                super.close();
+            }
+        };
+        return res;
+    }
+
+    private File getFile(Workbook workbook) throws IOException {
+        Sheet sheet = workbook.createSheet("졸업 대상자 조회");
+        int rowNo = 0;
+        CellStyle headStyle = getHeadStyle(workbook);
+        rowNo = createHeader(sheet, rowNo, headStyle);
+        createBody(sheet, rowNo);
+        setColumnSize(sheet);
+        File tmpFile = File.createTempFile("TMP~", ".xlsx");
+        return tmpFile;
+    }
+
+    private static void setColumnSize(Sheet sheet) {
+        sheet.setColumnWidth(0, 3000);
+        sheet.setColumnWidth(1, 3000);
+        sheet.setColumnWidth(2, 3000);
+        sheet.setColumnWidth(3, 3000);
+        sheet.setColumnWidth(4, 3000);
+        sheet.setColumnWidth(5, 3000);
+        sheet.setColumnWidth(6, 3000);
+        sheet.setColumnWidth(7, 3500);
+    }
+
+    private void createBody(Sheet sheet, int rowNo) {
+        List<ExcelBoard> dataList = excelBoardService.findExcelList();
+        for (ExcelBoard excelBoard : dataList) {
+            Row row = sheet.createRow(rowNo++);
+            row.createCell(0).setCellValue(excelBoard.getStudentId());
+            row.createCell(1).setCellValue(excelBoard.getStudentName());
+            row.createCell(2).setCellValue(excelBoard.getProfessorName());
+            row.createCell(3).setCellValue(excelBoard.getGraduationDate());
+            row.createCell(4).setCellValue(excelBoard.getStep());
+            row.createCell(5).setCellValue(excelBoard.getState());
+            row.createCell(6).setCellValue(excelBoard.getQualifications());
+            row.createCell(7).setCellValue(excelBoard.getCapstoneCompletion());
+        }
+    }
+
+    private static int createHeader(Sheet sheet, int rowNo, CellStyle headStyle) {
+        Row headerRow = sheet.createRow(rowNo++);
+        headerRow.createCell(0).setCellValue("학번");
+        headerRow.createCell(1).setCellValue("학생 이름");
+        headerRow.createCell(2).setCellValue("교수 이름");
+        headerRow.createCell(3).setCellValue("졸업 날짜");
+        headerRow.createCell(4).setCellValue("단계");
+        headerRow.createCell(5).setCellValue("상태");
+        headerRow.createCell(6).setCellValue("기타 자격");
+        headerRow.createCell(7).setCellValue("캡스톤 이수");
+
+        for (int i = 0; i <= 7; i++) {
+            headerRow.getCell(i).setCellStyle(headStyle);
+        }
+        return rowNo;
+    }
+
+    private static CellStyle getHeadStyle(Workbook workbook) {
+        CellStyle headStyle = workbook.createCellStyle();
+        headStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_ORANGE.getIndex());
+        headStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        Font font = workbook.createFont();
+        font.setColor(HSSFColor.HSSFColorPredefined.WHITE.getIndex());
+        font.setFontHeightInPoints((short) 13);
+        headStyle.setFont(font);
+        return headStyle;
+    }
     private void approveExcelAndSubmitForm(ExcelBoardSubmitFormDto excelBoardSubmitFormDto, Users user) {
         if (!Objects.isNull(user.getSubmitForm())) {
             SubmitForm submitFormId = submitFormService.findSubmitForm(user.getSubmitForm().getId());
