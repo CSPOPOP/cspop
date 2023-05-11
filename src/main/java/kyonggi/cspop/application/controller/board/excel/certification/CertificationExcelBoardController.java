a88feff184e34ef09ba2a8b3f30d286bd208a5ff
kw99j10 <foryou0084@gmail.com>
2023-05-11 20:07:42 +0900

    refactor: CertificationExcelBoardController 에 ExcelStore 적용

--- src/main/java/kyonggi/cspop/application/controller/board/excel/certification/CertificationExcelBoardController.java
+++ src/main/java/kyonggi/cspop/application/controller/board/excel/certification/CertificationExcelBoardController.java
@@ -1,22 +1,15 @@
 package kyonggi.cspop.application.controller.board.excel.certification;
 
+import kyonggi.cspop.application.util.ExcelStore;
 import kyonggi.cspop.application.util.PageStore;
 import kyonggi.cspop.domain.board.certification.CertificationBoard;
 import kyonggi.cspop.domain.board.certification.dto.CertificationBoardResponseDto;
 import kyonggi.cspop.domain.board.certification.service.CertificationBoardService;
-import kyonggi.cspop.exception.CsPopErrorCode;
-import kyonggi.cspop.exception.CsPopException;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.util.HSSFColor;
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,11 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
@@ -39,6 +29,7 @@ public class CertificationExcelBoardController {
 
     private final CertificationBoardService certificationBoardService;
     private final PageStore pageStore;
+    private final ExcelStore excelStore;
 
     @GetMapping("/certification_management")
     public String certificationForm(Pageable pageable, Model model) {
@@ -50,19 +41,14 @@ public class CertificationExcelBoardController {
 
     @PostMapping("/certification_management.read")
     public String uploadCertification(@RequestParam("file") MultipartFile file, Model model) throws IOException {
-        List<CertificationBoard> certificationBoardList = checkExcelFile(file);
-        certificationBoardService.deleteExcelListAndUploadCertificationList(certificationBoardList);
-        model.addAttribute("certification", certificationBoardList);
+        deleteExistentFileAndUploadCertificationExcelFile(file, model);
         return "redirect:./certification_management?page=0&size=10";
     }
 
     @SneakyThrows
     @GetMapping("/certification_management.download")
-    public ResponseEntity<InputStreamResource> downloadCertification(HttpServletResponse response) {
-        File tmpFile = getTmpFile();
-        InputStream excelFile = getExcelFile(tmpFile);
-        return ResponseEntity.ok().contentLength(tmpFile.length()).contentType(MediaType.APPLICATION_OCTET_STREAM).header("Content-Disposition", "attachment;filename=certification.xlsx")
-                .body(new InputStreamResource(excelFile));
+    public ResponseEntity<InputStreamResource> downloadCertificationExcelFile() {
+        return excelStore.downloadCertificationExcelFile();
     }
 
     private static void putPagingInf(Model model, Page<CertificationBoardResponseDto> allCertificationBoard, int[] startAndEndBlockPage) {
@@ -71,199 +57,9 @@ public class CertificationExcelBoardController {
         model.addAttribute("certification", allCertificationBoard);
     }
 
-    private static List<CertificationBoard> checkExcelFile(MultipartFile file) throws IOException {
-        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
-        checkUploadCertificationFileExtension(extension);
-        Sheet worksheet = getWorksheet(file, extension);
-        return getCertificationList(worksheet);
-    }
-
-    private File getTmpFile() throws IOException {
-        Workbook workbook = new XSSFWorkbook();
-        File tmpFile = getFile(workbook);
-        OutputStream fos = new FileOutputStream(tmpFile);
-        workbook.write(fos);
-        return tmpFile;
-    }
-
-    private static InputStream getExcelFile(File tmpFile) throws FileNotFoundException {
-        InputStream res = new FileInputStream(tmpFile) {
-            @Override
-            public void close() throws IOException {
-                super.close();
-            }
-        };
-        return res;
-    }
-
-    private File getFile(Workbook workbook) throws IOException {
-        Sheet sheet = workbook.createSheet("공학인증 사정 조회");
-        int rowNo = 0;
-        CellStyle headStyle = getHeadStyle(workbook);
-        rowNo = createHeader(sheet, rowNo, headStyle);
-        createBody(sheet, rowNo);
-        setColumnSize(sheet);
-        File tmpFile = File.createTempFile("TMP~", ".xlsx");
-        return tmpFile;
-    }
-
-    private static void setColumnSize(Sheet sheet) {
-        sheet.setColumnWidth(0, 3500);
-        sheet.setColumnWidth(1, 3000);
-        sheet.setColumnWidth(2, 3000);
-        sheet.setColumnWidth(3, 3000);
-        sheet.setColumnWidth(4, 2500);
-        sheet.setColumnWidth(5, 2500);
-        sheet.setColumnWidth(6, 2500);
-        sheet.setColumnWidth(7, 2500);
-        sheet.setColumnWidth(8, 2500);
-        sheet.setColumnWidth(9, 2500);
-        sheet.setColumnWidth(10, 2500);
-        sheet.setColumnWidth(11, 8000);
-    }
-
-    private void createBody(Sheet sheet, int rowNo) {
-        List<CertificationBoard> dataList = certificationBoardService.findCertificationList();
-        for (CertificationBoard certificationBoard : dataList) {
-            Row row = sheet.createRow(rowNo++);
-            row.createCell(0).setCellValue(certificationBoard.getDepartment());
-            row.createCell(1).setCellValue(certificationBoard.getStudentId());
-            row.createCell(2).setCellValue(certificationBoard.getStudentName());
-            row.createCell(3).setCellValue(certificationBoard.getCurrentSemester());
-            row.createCell(4).setCellValue(certificationBoard.getProfessionalEducation());
-            row.createCell(5).setCellValue(certificationBoard.getMscBsm());
-            row.createCell(6).setCellValue(certificationBoard.getDesign());
-            row.createCell(7).setCellValue(certificationBoard.getMajor());
-            row.createCell(8).setCellValue(certificationBoard.getEssential());
-            row.createCell(9).setCellValue(certificationBoard.getFirstAndLast());
-            row.createCell(10).setCellValue(certificationBoard.getTotal());
-            row.createCell(11).setCellValue(certificationBoard.getSpecialNote());
-
-        }
-    }
-
-    private static int createHeader(Sheet sheet, int rowNo, CellStyle headStyle) {
-        Row headerRow = sheet.createRow(rowNo++);
-        headerRow.createCell(0).setCellValue("소속 학과");
-        headerRow.createCell(1).setCellValue("학번");
-        headerRow.createCell(2).setCellValue("학생 이름");
-        headerRow.createCell(3).setCellValue("현재 학기");
-        headerRow.createCell(4).setCellValue("전문교양");
-        headerRow.createCell(5).setCellValue("MSC/BSM");
-        headerRow.createCell(6).setCellValue("설계");
-        headerRow.createCell(7).setCellValue("전공");
-        headerRow.createCell(8).setCellValue("필수");
-        headerRow.createCell(9).setCellValue("선/후수");
-        headerRow.createCell(10).setCellValue("총 학점");
-        headerRow.createCell(11).setCellValue("특이 사항");
-
-        for (int i = 0; i <= 11; i++) {
-            headerRow.getCell(i).setCellStyle(headStyle);
-        }
-        return rowNo;
-    }
-
-    private static CellStyle getHeadStyle(Workbook workbook) {
-        CellStyle headStyle = workbook.createCellStyle();
-        headStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_ORANGE.getIndex());
-        headStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-        Font font = workbook.createFont();
-        font.setColor(HSSFColor.HSSFColorPredefined.WHITE.getIndex());
-        font.setFontHeightInPoints((short) 13);
-        headStyle.setFont(font);
-        return headStyle;
-    }
-
-    private static List<CertificationBoard> getCertificationList(Sheet worksheet) {
-        List<CertificationBoard> certificationBoardList = new ArrayList<>();
-        for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) {
-            Row row = worksheet.getRow(i);
-            for (int j = 0; j < row.getLastCellNum(); j++) {
-                Cell cell = row.getCell(j);
-                StringBuilder row1 = new StringBuilder();
-                if (cell == null) {
-                    String value = "N/A";
-                    cell = row.createCell(j);
-                    cell.setCellValue(value);
-                } else if (cell.getCellType() == CellType.BLANK) {
-                    String value = "N/A";
-                    cell.setCellValue(value);
-                } else if (cell.getCellType() == CellType.STRING) {
-                    String value = cell.getStringCellValue();
-                    if (value == null || value.trim().isEmpty()) {
-                        String replacementValue = "N/A";
-                        cell.setCellValue(replacementValue);
-                    }
-                } else if (cell.getCellType() == CellType.NUMERIC) {
-                    double value = cell.getNumericCellValue();
-                    String stringValue = String.valueOf(value);
-
-                    if (stringValue.trim().isEmpty()) {
-                        String replacementValue = "N/A";
-                        cell.setCellValue(replacementValue);
-                    } else {
-                        if (stringValue.length() > 8) {
-                            for (int k = 0; k < stringValue.length(); k++) {
-                                char c = stringValue.charAt(k);
-
-                                if (c == '.') continue;
-
-                                if (c >= 'A' && c <= 'Z') break;
-
-                                row1.append(c);
-                            }
-                            if (row1.length() == 8) {
-                                row1.append("0");
-                            } else if (row1.length() == 7) {
-                                row1.append("00");
-                            } else if (row1.length() == 6) {
-                                row1.append("000");
-                            }
-                        } else {
-                            if (!stringValue.contains(".0")) {
-                                row1.append(stringValue);
-                            } else {
-                                for (int k = 0; k < stringValue.length(); k++) {
-                                    char c = stringValue.charAt(k);
-
-                                    if (c == '.') break;
-
-                                    row1.append(c);
-                                }
-                            }
-                        }
-                    }
-                    cell.setCellValue(row1.toString());
-                }
-            }
-            CertificationBoard data = CertificationBoard.createCertificationBoard(row);
-            certificationBoardList.add(data);
-        }
-        return certificationBoardList;
-    }
-
-    private static Sheet getWorksheet(MultipartFile file, String extension) throws IOException {
-        Workbook workbook = getWorkbook(file, extension);
-        Sheet worksheet = Objects.requireNonNull(workbook).getSheetAt(0);
-        return worksheet;
-    }
-
-    private static Workbook getWorkbook(MultipartFile file, String extension) throws IOException {
-        Workbook workbook = null;
-        if (extension.equals("xlsx")) {
-            workbook = new XSSFWorkbook(file.getInputStream());
-        } else if (extension.equals("xls")) {
-            workbook = new HSSFWorkbook(file.getInputStream());
-        }
-        return workbook;
-    }
-
-    private static void checkUploadCertificationFileExtension(String extension) {
-        if (extension.equals("")) {
-            throw new CsPopException(CsPopErrorCode.NO_UPLOAD_FILE_EXTENSION);
-        }
-        if (!extension.equals("xlsx") && !extension.equals("xls")) {
-            throw new CsPopException(CsPopErrorCode.INVALID_UPLOAD_FILE_EXTENSION);
-        }
+    private void deleteExistentFileAndUploadCertificationExcelFile(MultipartFile file, Model model) throws IOException {
+        List<CertificationBoard> certificationBoardList = excelStore.checkCertificationExcelFile(file);
+        certificationBoardService.deleteExcelListAndUploadCertificationList(certificationBoardList);
+        model.addAttribute("certification", certificationBoardList);
     }
 }
