12bf18dfe4a4341f7a25f5d2fac70469f3161b55
HamHyeonJun <hyunjun003@naver.com>
2023-05-30 09:38:34 +0900

    feat: data 보여주는 card 추가 - 새로운 메세지가 생길시 최하단으로 스크롤을 내리는 js 추가

--- src/main/webapp/assets/js/chatBot.js
+++ src/main/webapp/assets/js/chatBot.js
@@ -1,5 +1,3 @@
-
-
 const userInput = $('#user-input');
 const messageArea = $('#message-area');
 userInput.on('keyup', (event) => {
@@ -8,13 +6,26 @@ userInput.on('keyup', (event) => {
 
         if (userMessage !== '') {
             messageArea.append('<div class="message user-message">' + userMessage + '</div>');
+            scrollToBottom()
             userInput.val('');
         }
     }
 });
 
+function scrollToBottom() {
+    const chatContainer = $('#message-area');
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
 function chatBotAjax(mode) {
-    messageArea.append('<div class="message user-message">' + mode + ' 알려줘</div>');
+    messageArea.append(
+        '<div class="card float-left container" style="width: 18rem;">\n' +
+        '  <div class="card-body">\n' +
+        '    <h5 class="card-title">Card title</h5>\n' +
+        '    <p class="card-text">' + mode + '</p>\n' +
+        '  </div>\n' +
+        '</div>'
+    );
     let jsonData = {
         "sentence": mode
     }
@@ -23,7 +34,17 @@ function chatBotAjax(mode) {
         url: "/api/chatBot",
         contentType: "application/json; charset=utf-8",
         data: JSON.stringify(jsonData),
-        success: (data) => messageArea.append('<div class="message user-message">' + data.answer + '</div>'),
+        success: (data) => {
+            // messageArea.append('<div class="message user-message">' + data.answer + '</div>')
+            messageArea.append(
+                '<div class="card float-right container" style="width: 18rem;">\n' +
+                '  <div class="card-body">\n' +
+                '    <h5 class="card-title">ChatBot</h5>\n' +
+                '    <p class="card-text">' + data.answer + '</p>\n' +
+                '  </div>\n' +
+                '</div>')
+            scrollToBottom()
+        },
         error: (error) => alert("유효하지 않은 접근 입니다")
     })
 }
