ce60ebdee3e71818be62d166b7c3182f721497d3
kw99j10 <foryou0084@gmail.com>
2023-05-31 17:20:41 +0900

    feat: 스케쥴링 예외처리 추가 - 진행일정 시작일,종료일 null 예외 처리 - 종료일이 시작일보다 앞설 시 예외 처리

--- src/main/java/kyonggi/cspop/application/controller/board/schedule/ScheduleController.java
+++ src/main/java/kyonggi/cspop/application/controller/board/schedule/ScheduleController.java
@@ -6,10 +6,14 @@ import kyonggi.cspop.domain.board.schedule.ScheduleBoard;
 import kyonggi.cspop.domain.board.schedule.service.ScheduleBoardService;
 import kyonggi.cspop.domain.schedule.Schedules;
 import kyonggi.cspop.domain.schedule.service.ScheduleService;
+import kyonggi.cspop.exception.CsPopErrorCode;
+import kyonggi.cspop.exception.CsPopException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -68,7 +72,13 @@ public class ScheduleController {
     }
 
     @PostMapping("/schedule/modify/{id}")
-    public ResponseEntity<Void> scheduleModify(@PathVariable Long id, @RequestBody ScheduleDto scheduleDto) {
+    public ResponseEntity<Void> scheduleModify(@PathVariable Long id, @Validated @RequestBody ScheduleDto scheduleDto) {
+        if (scheduleDto.getEndDate() == null || scheduleDto.getStartDate() == null) {
+            throw new CsPopException(CsPopErrorCode.SCHEDULE_HAS_NULL_CONTENT);
+        }
+        if (scheduleDto.getEndDate().isBefore(scheduleDto.getStartDate())) {
+            throw new CsPopException(CsPopErrorCode.SCHEDULE_HAS_INVALID_DATE);
+        }
         scheduleService.updateSchedules(id, scheduleDto);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/kyonggi/cspop/exception/CsPopErrorCode.java
+++ src/main/java/kyonggi/cspop/exception/CsPopErrorCode.java
@@ -19,6 +19,10 @@ public enum CsPopErrorCode {
     //NoticeBoard
     NOTICE_BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "게시판이 이미 삭제 되었습니다."),
 
+    //Schedule
+    SCHEDULE_HAS_NULL_CONTENT(HttpStatus.NOT_FOUND,"날짜를 지정해야 합니다."),
+    SCHEDULE_HAS_INVALID_DATE(HttpStatus.BAD_REQUEST,"시작 날짜는 종료 날짜보다 앞설 수 없습니다."),
+
     //Form
     FORM_HAS_NULL_CONTENT(HttpStatus.NOT_FOUND,"비어있는 항목이 있어 제출할 수 없습니다."),
     SUBMIT_NOT_QUALIFICATION(HttpStatus.BAD_REQUEST,"존재하지 않는 졸업 요건입니다."),
